dcb13855655ed70e5c9fb2b6d0ad3615aacf0f09
Kenny Daniel Diaz Diaz <kennydanieldiaz@gmail.com>
2025-09-29 09:54:43 -0500

    Refactor Automovil class for better structure
    Updated the Automovil class to fix method return types and improve code structure.

--- actividad02/ejercicio03/Automovil.java
+++ actividad02/ejercicio03/Automovil.java
@@ -119,7 +119,7 @@ public class Automovil {
 
     }
 
-    int tiempoLlegada(int distancia) {
+    double tiempoLlegada(int distancia) {
         if (velocidadActual > 0) {
             return distancia / velocidadActual;
         } else {
@@ -145,3 +145,4 @@ public class Automovil {
         System.out.println("Color: " + color + "\n");
     }   
 }
+
